bef1e5f34fdd4c0312c23f5bc105eb924a8357f4
BrendaSaucedo12 <88901374+BrendaSaucedo12@users.noreply.github.com>
2022-04-12 16:48:15 -0500

    Lee el archivo

--- Actividad 3.2/act3.2.py
+++ Actividad 3.2/act3.2.py
@@ -1,1 +1,46 @@
+'''
+Actividad 3.2 - Programando un DFA
 
+Fecha: 22-04-2020
+
+Equipo:
+    - 
+    -
+    - Brenda Elena Saucedo González | A00829855
+'''
+
+# Definición de función principal
+def lexerAritmetico(nombre_archivo):
+    
+    # Abrir el archivo de expresiones
+    with open (nombre_archivo, mode = 'r', encoding = "utf-8") as archivo:
+        # Leer todo el archivo y dejarlo en una lista de strings
+        lista_expresion = archivo.readlines()
+        
+    # Lee cada enunciado del archivo
+    for num, expresion in enumerate(lista_expresion):
+        print(expresion)
+        print ("Token\tTipo")
+        
+        # Lee cada caracter del enunciado
+        for num2, token in enumerate(expresion):
+            if token.isalpha():
+                print(str(token) + "\tVariable")
+            elif token == "=":
+                print(str(token) + "\tAsignación")
+            elif token.isdigit():
+                print(str(token) + "\tEntero")
+            
+            
+        print("\n")
+    
+    
+# Definición de la función main
+def main():
+    # Se llama a la función principal
+    lexerAritmetico("expresiones.txt")
+    
+    
+    
+# Llamar a la funcion main
+main()
