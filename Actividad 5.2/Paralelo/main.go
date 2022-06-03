b2c4759d390681a0c8789b640940608b0f3bb2df
5100-chap <A01275100@itesm.mx>
2022-06-03 17:21:46 -0500

    Programa paralelo terminado y corregido junto con comentarios y correcciones a parte secuencial

--- Actividad 5.2/Paralelo/main.go
+++ Actividad 5.2/Paralelo/main.go
@@ -13,14 +13,13 @@ de un programa que calcule la suma de todos los n ́umeros primos menores a 5,00
 • La segunda versi ́on debe realizar el cómputo de manera paralela a trav ́es de los mecanismos provistos por
 el lenguaje siendo utilizado (por ejemplo places o la funci ́on pmap). Debes procurar paralelizar el c ́odigo
 aprovechando todos los n ́ucleos disponibles en tu sistema.
-Ambas versiones del programa deben dar 838,596,693,108 como resultado.*/
+Ambas versiones del programa deben dar 838,596,693,108 (838596693108) como resultado.*/
 
 package main
 
 import (
 	"fmt"
 	"math"
-	"runtime"
 	"sync"
 )
 
@@ -28,8 +27,8 @@ import (
 var n float64
 var wg sync.WaitGroup
 var mutex = &sync.Mutex{}
-var sum vector
 
+//Declaracion de tipo de dato
 type vector []int
 
 //Checa si los numeros son primos
@@ -46,22 +45,25 @@ func check_prime(n float64) bool {
 }
 
 //Funcion para sumar los numeros primos
-func sec_prime(begining, ending, step int, ch chan vector) {
+func sec_prime(begining, ending, step int, ch chan int) {
+	var sum int
 	for i := begining; i < ending; i += step {
 		if check_prime(float64(i)) == true {
 			mutex.Lock()
-			sum = append(sum, i)
+			sum += i
 			mutex.Unlock()
 		}
 	}
 	ch <- sum
 }
 
-/*Encargado de dividir el tiempo entre el numero de hilos asignados, con
-el fin de aumentar la velocidad del chequeo*/
+/*Encargado de dividir el numero entre el numero de procesos asignados, con
+el fin de adecuar las goroutines al numero de procesos asignado*/
 func rango_div(hilos, limite int) vector {
 	var rango vector
+	//Nuestro valor inicial para el rango es 0
 	rango = append(rango, 0)
+	//Base para poder definir el rango
 	unidad := limite / hilos
 	for i := 1; i < hilos+1; i++ {
 		if i >= hilos && i*unidad != limite {
@@ -74,15 +76,35 @@ func rango_div(hilos, limite int) vector {
 }
 
 func main() {
-	hilos := runtime.NumCPU()
-	n := 10
-	vecCh := make(chan vector)
-	var result vector
+	//Declaramos las variables a usar
+	//Hilos sera el numero de procesos a usar
+	hilos := 1024
+	//n sera el numero a analizar
+	n := 5000000
+	// sumCh se usa para poder pasar la suma de las goroutines
+	sumCh := make(chan int)
+	//Resultado recibira la información del canal sumCh
+	var result int
+	//Calculamos el rango
 	div_range := rango_div(hilos, n)
+	//Llamamos a los goroutines con el numero de procesos
 	for i := 0; i < hilos; i++ {
-		go sec_prime(div_range[i], div_range[(i+1)], 1, vecCh)
-		e := <-vecCh
-		result = append(e)
+		go sec_prime(div_range[i], div_range[(i+1)], 1, sumCh)
 	}
-	fmt.Println(result[0])
+	//Sumamos lo que tenga sumCh
+	for i := 0; i < hilos; i++ {
+		e := <-sumCh
+		result += e
+	}
+	//Imprimimos el resultado
+	fmt.Println(result)
 }
+
+/*Reporte de Diego:
+En un Ryzen 5 3500X en un nucleo el programa (Metodo paralelo) termino en
+64.424 segundos
+Salida de consola:
+
+[Running] go run "e:\Seagate_4tb\Documentos\Github_clone\Equipo-Racket-1\Actividad 5.2\Paralelo\main.go"
+838596693108
+[Done] exited with code=0 in 11.894 seconds*/
--- Actividad 5.2/Secuencial/main.go
+++ Actividad 5.2/Secuencial/main.go
@@ -13,7 +13,7 @@ de un programa que calcule la suma de todos los n ́umeros primos menores a 5,00
 • La segunda versi ́on debe realizar el cómputo de manera paralela a trav ́es de los mecanismos provistos por
 el lenguaje siendo utilizado (por ejemplo places o la funci ́on pmap). Debes procurar paralelizar el c ́odigo
 aprovechando todos los n ́ucleos disponibles en tu sistema.
-Ambas versiones del programa deben dar 838,596,693,108 como resultado.*/
+Ambas versiones del programa deben dar 838,596,693,108 (838596693108) como resultado.*/
 
 package main
 
@@ -27,6 +27,9 @@ var n float64
 
 //Checa si los numeros son primos
 func check_prime(n float64) bool {
+	if n <= 1 {
+		return false
+	}
 	for i := 2.0; i <= math.Sqrt(n); i += 1.0 {
 		if math.Mod(n, i) == 0 {
 			return false
@@ -47,22 +50,17 @@ func sec_prime(limit int) int {
 }
 
 func main() {
+	//n sera el numero a analizar
 	n := 5000000
+	//imprimimos el resultado
 	fmt.Println(sec_prime(n))
 	/*Reporte de Diego:
 	En un Ryzen 5 3500X en un nucleo el programa (Metodo secuencial) termino en
 	64.424 segundos
 	Salida de consola:
 
-	[Running] go run "e:\Seagate_4tb\Documentos\Github_clone\Equipo-Racket-1\Actividad 5.2\main.go"
+	[Running] go run "e:\Seagate_4tb\Documentos\Github_clone\Equipo-Racket-1\Actividad 5.2\Secuencial\main.go"
 	838596693108
-	[Done] exited with code=0 in 64.424 seconds
-
-	En un Intel Core i5-11400H en un nucleo el programa (Metodo secuencial) termino en
-	47.425 segundos
-	Salida de consola:
-	[Running] go run "c:\Users\Upijijis\OneDrive\Documentos\git\Go\Act 5.2\main.go"
-	838596693108
-	[Done] exited with code=0 in 47.425 seconds
+	[Done] exited with code=0 in 62.389 seconds
 	*/
 }
